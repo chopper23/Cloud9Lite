81e6f7bcfaa326d8b5e31614bb23d831fa32cc0f
chopper23 <chopperwalker@yahoo.com>
2017-08-06 01:27:43 -0500

    Including sound control

--- static/script.js
+++ static/script.js
@@ -189,6 +189,7 @@
 			}
 			
 			function invertColor(hex) {
+				bw = true
 				if (hex.indexOf('#') === 0) {
 					hex = hex.slice(1);
 				}
@@ -199,12 +200,21 @@
 				if (hex.length !== 6) {
 					throw new Error('Invalid HEX color.');
 				}
+				var r = parseInt(hex.slice(0, 2), 16),
+					g = parseInt(hex.slice(2, 4), 16),
+					b = parseInt(hex.slice(4, 6), 16);
+				if (bw) {
+					// http://stackoverflow.com/a/3943023/112731
+					return (r * 0.299 + g * 0.587 + b * 0.114) > 186
+						? '#000000'
+						: '#FFFFFF';
+				}
 				// invert color components
-				var r = (255 - parseInt(hex.slice(0, 2), 16)).toString(16),
-					g = (255 - parseInt(hex.slice(2, 4), 16)).toString(16),
-					b = (255 - parseInt(hex.slice(4, 6), 16)).toString(16);
+				r = (255 - r).toString(16);
+				g = (255 - g).toString(16);
+				b = (255 - b).toString(16);
 				// pad each with zeros and return
-				return '#' + padZero(r) + padZero(g) + padZero(b);
+				return "#" + padZero(r) + padZero(g) + padZero(b);
 			}
 
 			function padZero(str, len) {
